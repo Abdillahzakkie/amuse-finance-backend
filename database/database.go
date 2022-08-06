a17e1f73a0830ed39abf197ab48e6a46c3f56a46
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-08-06 11:07:26 +0100

    modified NewClient function to take in the connection string

--- database/database.go
+++ database/database.go
@@ -3,21 +3,15 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
+	"github.com/abdillahzakkie/amuse-finance-backend/helpers"
 	_ "github.com/lib/pq"
 )
 
 // NewClient takes the environment variables and uses them to connect to the database
-func NewClient() (*sql.DB, error) {
-	host := os.Getenv("HOST_NAME")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	
+func NewClient(psqlInfo string) (*sql.DB, error) {
+	// load .env file to environment variables
+	helpers.LoadEnv()
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
