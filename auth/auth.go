7eab0e0a22a7ee8e7f3de462158408eb13ddad56
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-08-07 11:59:27 +0100

    GenerateJwtToken generates a new JWT token using the username passed in

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,23 @@
 package auth
 
 import (
+	"errors"
 	"os"
+	"time"
 
 	"github.com/abdillahzakkie/amuse-finance-backend/validators"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var (
+	ErrInvalidJWT = errors.New("invalid token")
+)
+
+type customClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
 
 // HashPassword hashed the provided password
 // returns the hashed password or ErrInvalidPassword if an error is encountered.
@@ -18,4 +30,20 @@ func HashPassword(password string) (string, error) {
 		return "", validators.ErrInvalidPassword
 	}
 	return string(hashedPassword), nil
+}
+
+
+func GenerateJwtToken(username string) (string, error) {
+	claims := customClaims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SIGNING_KEY")))
+	if err != nil {
+		return "", validators.ErrInternalServerError
+	}
+	return signedToken, nil
 }
