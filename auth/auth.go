9f1b1047906e41f0a2c9792c6ae927dfd031faa5
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-08-07 07:16:06 +0100

    HashPassword hashes the provided password using bycrypt and returns the new hash or an error if one is encountered

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,21 @@
 package auth
+
+import (
+	"os"
+
+	"github.com/abdillahzakkie/amuse-finance-backend/validators"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashed the provided password
+// returns the hashed password or ErrInvalidPassword if an error is encountered.
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(password + os.Getenv("PASSWORD_HASH_SECRET")), 
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return "", validators.ErrInvalidPassword
+	}
+	return string(hashedPassword), nil
+}
