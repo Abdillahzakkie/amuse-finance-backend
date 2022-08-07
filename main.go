552a51cb743afa80fb37200b63b00f3d2e3239c5
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-08-07 12:07:17 +0100

    added config function to configure logger, cors, cookies middleware options on the server instance

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/abdillahzakkie/amuse-finance-backend/routes"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
@@ -23,10 +26,33 @@ func main() {
 
 	port := fmt.Sprintf("localhost:%s", os.Getenv("PORT"))
 
+	config(app, port)
 	setupRoutes(app)
 
 	log.Println(fmt.Sprintln("Server listening on port:", port))
 	log.Fatal(app.Listen(port))
+}
+
+func config(app *fiber.App, port string) {
+	app.Use(logger.New())
+	
+	app.Use(cors.New(cors.Config{
+		// AllowOrigins: "*",
+		AllowOrigins: port,
+		AllowHeaders:  "Origin, Content-Type, Accept",
+	}))
+
+	// app.Use(csrf.New(csrf.Config{
+	// 	KeyLookup:      "header:X-Csrf-Token",
+	// 	CookieName:     "csrf_",
+	// 	CookieSameSite: "Strict",
+	// 	CookieHTTPOnly: true,
+	// 	Expiration:     time.Hour * 24,
+	// }))
+
+	app.Use(encryptcookie.New(encryptcookie.Config{
+		Key: os.Getenv("COOKIES_PASSPHRASE"),
+	}))
 }
 
 func setupRoutes(app *fiber.App) {
